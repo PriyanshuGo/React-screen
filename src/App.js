e13adb98d46fde6b3ca6928d69d8c09dfcb28166
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2023-08-01 21:55:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Head from "./component/Head";
+import Boy from "./component/Boy";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <Boy />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Boy.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import mobile from "../Assets/mobile.jpg";
+import apple from "../Assets/apple.png";
+import google from "../Assets/google-play.png";
+import boss from "./boy.css";
+
+function Boy() {
+  return (
+    <>
+      <div className="body">
+        <div>
+          <p className="cont">Connecting all your banking needs</p>
+          <div>
+            <p className="cont1">
+              A smart mobile application you can control your business needs
+            </p>
+          </div>
+          {/* <div className="btnclass">
+          <button className="btn1"><p className="btntext">Download App</p></button>
+        <button className="btn2"><p className="btntext">Download App</p></button>
+          </div> */}
+          <div className="foot">
+            <button >
+              <img src={apple} className="space" />
+              Download App
+            </button>
+            <button className="btn2">
+              <img src={google} className="space" />
+              Download App
+            </button>
+          </div>
+        </div>
+        <div className="mobile">
+          <img src={mobile} width={754} height={502} />
+        </div>
+     
+      </div>
+    </>
+  );
+}
+
+export default Boy;
